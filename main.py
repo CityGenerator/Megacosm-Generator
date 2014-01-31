720bf4aa7f131f5cdf04c65d381f60d7371202d6
Jesse Morgan <morgajel@gmail.com>
2014-01-31 13:17:26 -0500

    still playing with layout stuff

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 
 
 # Import the stuffs!
-from flask import Flask, send_file, render_template, request
-from noise import snoise2, pnoise2,snoise3
+from flask import Flask, send_file, render_template, request, url_for
+from noise import snoise2, snoise3, snoise4
+from StringIO import StringIO
 import png
-import StringIO
 import random
 
 
@@ -13,30 +13,60 @@ import random
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def welcomepage():
     """This is the first page anyone sees."""
-    return render_template('map.html')
+    worldId= request.args.get('worldId')
+    if (worldId == None):
+        worldId=random.randint(1,100000)
+    return render_template('map.html', worldId=worldId)
 
 @app.route('/worldmap.png')
 def worldmap():
-    """Return a worldmap."""
+    """Generate a worldmap and return it."""
     worldId= request.args.get('worldId')
-
     random.seed(worldId)
-    
+    myMap=generate_map(worldId)
+
+    # Create a temp file for writing the image
+    img_io = StringIO()
+
+    # Convert the matrix of pixels into a png, then write it to the temp file
+    imagewriter = png.Writer(len(myMap[0])/3, len(myMap))
+    imagewriter.write(img_io,myMap) 
+
+    #ensure the image is properly flushed
+    img_io.seek(0)
+
+    return send_file(img_io, mimetype='image/png', cache_timeout=100)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, Nothing at this URL.', 404
+
+@app.errorhandler(500)
+def page_borked(e):
+    """Return a custom 500 error. Only hit when debugging is off."""
+    message="You Broke it!"
+    return message, 500
+
+def generate_map(worldId):
     offset=random.randint(1,100000)
     s = []
-    zoom=260.0
+    zoom=250.0
     waterline=145
     for x in xrange(500):
         row=[]
         for y in xrange (800):
             
-            pixel=snoise2(offset+x/zoom,offset+y/zoom,  4, 0.4,2.0, 500/zoom*4, 800/zoom, 1 )
-            #noise2(x, y, octaves=1, persistence=0.5, lacunarity=2.0, repeatx=None, repeaty=None, base=0.0) return simplex noise value for specified 2D coordinate.
-
+            pixel=snoise2(x/zoom+offset , y/zoom+offset,  4, 0.4,2.0, 500/zoom*4, 800/zoom, 1 )
+#            pixel=snoise3(x/zoom,y/zoom, offset,  4, 0.4, 2.0 )
+#            pixel=snoise4(x/zoom,y/zoom, offset,offset ,  4, 0.4, 2.0 )
+            # noise2(x, y,       octaves=1, persistence=0.5, lacunarity=2.0, repeatx=None, repeaty=None, base=0.0) return simplex noise value for specified 2D coordinate.
+            # noise3(x, y, z,    octaves=1, persistence=0.5, lacunarity=2.0)
+            # noise4(x, y, z, w, octaves=1, persistence=0.5, lacunarity=2.0)
             pixel=int((pixel+1)/2*255-1)
 
             if (pixel <waterline):
@@ -49,19 +79,7 @@ def worldmap():
                 row.append(54+pixel)
                 row.append(pixel)
         s.append(row)
-
-    #draw a simple grayscale gradient png
-    w = png.Writer(len(s[0])/3, len(s),greyscale=False, alpha=False)
-
-    img_io = StringIO.StringIO()
-    w.write(img_io,s) 
-    img_io.seek(0)
-    return send_file(img_io, mimetype='image/png')
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, Nothing at this URL.', 404
+    return s
 
 
 if __name__ == '__main__':
@@ -69,3 +87,4 @@ if __name__ == '__main__':
     app.run()
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server...except we do to get it to run without app engine/
+
