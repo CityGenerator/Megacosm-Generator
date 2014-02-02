5eec68ac5ada8a1a7381036a37b9f9ee6d795e4a
Jesse Morgan <morgajel@gmail.com>
2014-02-01 22:54:04 -0500

    Generating my first world name...

--- scripts/reimport_data.py
+++ scripts/reimport_data.py
@@ -14,20 +14,22 @@ server=redis.from_url(url)
 
 json_data=open('data/worldnames.json')
 data = json.load(json_data)
+json_data.close()
+
 
 #pprint(data)
 for feature in data:
     if (feature.find("_chance") != -1 ):
         server.set('worldname'+feature, data[feature])
     else:
-        featurelist=data[feature]
-        server.sadd('worldname'+feature, *featurelist)
+        featureset=data[feature]
+        server.ltrim('worldname'+feature, 0, 0 )
+        server.lpush('worldname'+feature, *featureset)
 
     
- 
-print server.srandmember('worldnamepre')+server.srandmember('worldnameroot')+server.srandmember('worldnamepost')
+print server.llen('worldnamepost') 
+#print server.srandmember('worldnamepre')+server.srandmember('worldnameroot')+server.srandmember('worldnamepost')
 
 
 
-json_data.close()
 
