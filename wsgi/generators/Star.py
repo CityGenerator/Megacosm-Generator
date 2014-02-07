a1a903daa2d4309823932c579a7b07c8fe0a5b84
Jesse Morgan <morgajel@gmail.com>
2014-02-07 10:34:24 -0500

    tests from last night

---
+++ wsgi/generators/Star.py
@@ -0,0 +1,14 @@
+
+
+class Star():
+    def __init__(self):
+        self.color = '0xff00ff'
+
+    def generate_name(self,worldId,server):
+        #random.seed(worldId)
+
+        #name = server.lindex('worldnamepre',  random.randint(0,server.llen('worldnamepre')-1 ))
+        #name+= server.lindex('worldnameroot', random.randint(0,server.llen('worldnameroot')-1 ))
+        #name+= server.lindex('worldnamepost', random.randint(0,server.llen('worldnamepost')-1 ))
+        return name
+
