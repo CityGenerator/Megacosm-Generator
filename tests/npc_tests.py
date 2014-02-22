cd614d4a3787ad739c217b282af90f44c3a83a59
Jesse Morgan <morgajel@gmail.com>
2014-02-22 01:07:23 -0500

    fun with humans

--- tests/npc_tests.py
+++ tests/npc_tests.py
@@ -5,6 +5,8 @@ from mock import MagicMock
 
 import redis
 import ConfigParser, os
+from util.Seeds import *
+
 
 config = ConfigParser.RawConfigParser()
 config.read('data/config.ini')
@@ -16,30 +18,31 @@ class TestNPC(unittest.TestCase):
         """  """
         
         self.redis=redis.from_url(url)
+        self.seed=set_seed( 3 )
 
-#    def test_races(self):
-#        """  """
-#        npc = NPC(self.redis )
-#        self.assertIn(npc.race, self.redis.lrange('race',0,-1))
-#
-#        with self.assertRaisesRegexp(Exception, "turkeys is not a valid race and has no associated data") as context:
-#            npc = NPC(self.redis, {'race':'turkeys'} )
-#
-#        npc = NPC(self.redis, { 'race':'human'})
-#        self.assertEqual(npc.race,'human')
-#
-#    def test_race_details(self):
-#        """  """
-#        npc = NPC(self.redis, { 'race':'human'})
-#        self.assertEqual(npc.race,'human')
-#        self.assertEqual(npc.details['name'],'Human')
-#        self.assertEqual(npc.details['size'],'medium')
-#        self.assertEqual(npc.details['description'],'quick growth and adaptability')
+    def test_races(self):
+        """  """
+        npc = NPC(self.redis )
+        self.assertIn(npc.race, self.redis.lrange('race',0,-1))
+
+        with self.assertRaisesRegexp(Exception, "turkeys is not a valid race and has no associated data") as context:
+            npc = NPC(self.redis, {'race':'turkeys'} )
+
+        npc = NPC(self.redis, { 'race':'human'})
+        self.assertEqual(npc.race,'human')
+
+    def test_race_details(self):
+        """  """
+        npc = NPC(self.redis, { 'race':'human'})
+        self.assertEqual(npc.race,'human')
+        self.assertEqual(npc.details['name'],'Human')
+        self.assertEqual(npc.details['size'],'medium')
+        self.assertEqual(npc.details['description'],'quick growth and adaptability')
     def test_names(self):
         """  """
-        npc = NPC(self.redis, { 'seed':9304369, 'race':'human'})
+        npc = NPC(self.redis, { 'race':'human'})
         self.assertEqual(npc.race,'human')
-        self.assertEqual(npc.name['full'],'human')
+        self.assertEqual(npc.name['full'],'Willic MacGilridge')
 
 if __name__ == '__main__':
     unittest.main()
