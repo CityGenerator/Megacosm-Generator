6844f1639b90e9da53a30eb892448cfc31aa4f4e
Jesse Morgan <morgajel@gmail.com>
2014-03-04 22:11:58 -0500

    Thanks Jeff for catching this

--- util/Seeds.py
+++ util/Seeds.py
@@ -1,11 +1,11 @@
 import random
-
+import re
 def set_seed(seed):
 
     #Make sure it's an Integer
 
     # What if the seed is empty? Lets make a new one!
-    if (seed is None):
+    if seed is None  or re.search("\D", seed) or  int(seed) <1 or int(seed) > 100000:
         seed=random.randint(1,100000)
 
     random.seed(float(seed))
