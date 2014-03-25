267c6fcc6f3e8c356ebb1c03bc9b7a28b2e3391b
Jesse Morgan <morgajel@gmail.com>
2014-03-25 17:06:01 -0400

    sanity save of the dungeon generator

--- megacosm.py
+++ megacosm.py
@@ -7,6 +7,7 @@ from generators import Wanted
 from generators import Resource
 from generators import Event
 from generators import JobPosting
+from generators import Dungeon
 from util.Seeds import *
 from util import Filters
 import random
@@ -408,6 +409,24 @@ def Wanted_Builder():
 
 
 #########################################################################
+
+@app.route('/dungeon')
+def GenerateDungeon():
+    """Generate a simple dungeon"""
+    features=feature_filter('dungeon')
+    dungeon=Dungeon.Dungeon(server,features)
+    return render_template('dungeon.html', dungeon=dungeon,jsondata=dungeon.floor.convert_to_json() )
+
+
+@app.route('/dungeon_builder')
+def Dungeon_Builder():
+    """Generate the basic data about a dungeon"""
+    paramlist,paramstring,paramset=builder_form_data('dungeon')
+    return render_template('generic_builder.html',paramlist=paramlist,paramstring=paramstring, paramset=paramset, name='dungeon') 
+
+
+#########################################################################
+
 
 @app.route('/country')
 def GenerateCountry():
