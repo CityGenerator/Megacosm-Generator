90de7d85387aba55ca17c4b3caeb949c109a06fd
Jesse Morgan <morgajel@gmail.com>
2014-04-09 18:18:00 -0400

    fixing dupe

--- megacosm.py
+++ megacosm.py
@@ -682,6 +682,7 @@ def roguedungeon_builder():
     return render_template('generic_builder.html', plist=plist, pstring=pstring, pset=pset, name=classname)
 
 
+
 #########################################################################
 
 
